f5cba3bae8445874452b14b1ed79ff06b9945165
Jose Chavira <joeychavira2016@gmail.com>
2019-09-03 03:29:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,13 @@ def nyc_pigeon_organizer(data)
   data.collect do |key, value|
     value.collect do |new_value, names|
       names.collect do |name|
-
+        if !pigeon[name]
+          pigeon[name] = {}
+        end
+        if !pigeon[name][key]
+          pigeon[name][key] = []
+        end
+        pigeon[name][key].push(new_value.to_s)
       end
     end
   end
