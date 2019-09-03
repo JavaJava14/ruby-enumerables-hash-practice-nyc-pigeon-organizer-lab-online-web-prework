73e27dca65a96f23529f8101702ef9c0ea46135c
Jose Chavira <joeychavira2016@gmail.com>
2019-09-03 03:52:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon = {}
+  data.collect do |key, value|
+    value.collect do |new_value, names|
+      names.collect do |name|
+        if !pigeon[name]
+          pigeon[name] = {}
+        end
+        if !pigeon[name][key]
+          pigeon[name][key] = []
+        end
+        pigeon[name][key].push(new_value.to_s)
+      end
+    end
+  end
+  pigeon
 end
